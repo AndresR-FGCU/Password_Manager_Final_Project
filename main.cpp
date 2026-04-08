72507edb1b06e53f6ffce4e382df51a1d56c110a
damennguyen1 <damennguyen1@gmail.com>
2026-04-08 14:41:56 -0400

    Update main.cpp
    added class with username + password and added display of username + password

--- main.cpp
+++ main.cpp
@@ -3,15 +3,39 @@
 #include <vector>
 #include <fstream>
 #include <argon2.h>
+
 using namespace std;
 
-//Classes://
+class PasswordEntry {
+private:
+    string username;
+    string password;
+
+public:
+    PasswordEntry(string u, string p) : username(u), password(p) {
+    }
+
+    void display() {
+        cout << "Username: " << username << " | Password: " << password << endl;
+    }
+};
+
+int main() {
+
+    string usernames[5] = {};
+    string passwords[5] = {};
+
+    int i = 0;
 
 
+    cout << "Enter your Username: " << endl;
+    cin >> usernames[i];
+    cout << "Enter your Password: " << endl;
+    cin >> passwords[i];
 
-int main()
-{
-    cout << "Hello World!" << endl;
+    PasswordEntry myEntry(usernames[i],passwords[i]);
+        myEntry.display();
+
 
     return 0;
 }
